363c29362fe7e730b7b1b25879e906cb7a65dc8f
ozgenmuzac <ozgen@medrateknoloji.com>
2011-07-26 16:46:54 +0300

    redirfs

---
+++ redirfs-0.10/.redirfs.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ozgen/Desktop/packages/redirfs-0.10/redirfs.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-2.6.32-33-generic/scripts/module-common.lds --build-id -o /home/ozgen/Desktop/packages/redirfs-0.10/redirfs.ko /home/ozgen/Desktop/packages/redirfs-0.10/redirfs.o /home/ozgen/Desktop/packages/redirfs-0.10/redirfs.mod.o
